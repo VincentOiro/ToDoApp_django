aa69fcf4a0de152be54d5128d2b2afa45327d349
Vincent Oiro <vincentoiro1@gmail.com>
2024-04-14 19:42:03 +0300

    added delete,edit

--- todos/urls.py
+++ todos/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
-from . import views
+from. import views
+
+# This code defines URL patterns for a to-do list application using Django's path function.
+# Each URL pattern maps a URL to a view function and gives it a name for reverse URL resolving.
 
 urlpatterns = [
     # This URL pattern maps to the addTask view and gives it the name 'addTask'
+    # The URL for this pattern is '/addTask/'
     path('addTask/', views.addTask, name='addTask'),
+
     # This URL pattern maps to the mark_as_done view and gives it the name 'mark_as_done'
+    # The URL for this pattern is '/mark_as_done/<int:pk>/' where <int:pk> is the primary key of the task to be marked as done
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # This URL pattern maps to the mark_as_undone view and gives it the name 'mark_as_undone'
+    # The URL for this pattern is '/mark_as_undone/<int:pk>' where <int:pk> is the primary key of the task to be marked as undone
     path('mark_as_undone/<int:pk>',views.mark_as_undone, name='mark_as_undone'),
+
+    # This URL pattern maps to the edit_task view and gives it the name 'edit_task'
+    # The URL for this pattern is '/edit_task/<int:pk>/' where <int:pk> is the primary key of the task to be edited
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    # This URL pattern maps to the delete_task view and gives it the name 'delete_task'
+    # The URL for this pattern is '/delete_task/<int:pk>/' where <int:pk> is the primary key of the task to be deleted
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import redirect, get_object_or_404, render
+from django.http import HttpResponse 
 from .models import Task
 
 # This function creates a new task and saves it to the database
@@ -22,4 +22,24 @@ def mark_as_undone(request, pk):
     task.is_completed = False
     task.save()
     return  redirect('home')
+
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+       context = {
+           'get_task': get_task,
+       }
+       return render(request, 'edit_task.html', context)
+   
+   
+def delete_task(request, pk):
+   task = get_object_or_404(Task, pk=pk)
+   task.delete()
+   return redirect('home')
   
